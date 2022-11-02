c99132f4fa46f1afb63af7e1b87f0fdae5ced41a
Trevor <trevorgrilley@yahoo.com>
2022-11-01 20:37:29 -0400

    add prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,25 +1,96 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const genRead = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-//Project title
+inquirer.prompt(
+    [
+        //Project title
+        {
+            type: 'input',
+            message: 'Enter project title',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Project description
+        //Project description
+        {
+            type: 'input',
+            message: 'Enter project description',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Installation instructions
+        //Installation instructions
+        {
+            type: 'input',
+            message: 'Enter installation instructions',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Usage information
+        //Usage information
+        {
+            type: 'input',
+            message: 'Enter usage information',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Contribution Guidelines
+        //Contribution Guidelines
+        {
+            type: 'input',
+            message: 'Enter contribution guidelines',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Test instructions
+        //Test instructions
+        {
+            type: 'input',
+            message: 'Enter test instructions',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Licenses
+        //Licenses
+        {
+            type: 'input',
+            message: 'Enter licenses used',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Github Username
+        //Github Username
+        {
+            type: 'input',
+            message: 'Enter github username',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
 
-//Email
+        //Email
+        {
+            type: 'input',
+            message: 'Enter email',
+            name: 'title',
+            //validation to check if user entered a proper response
+            validate: (value)=>{ if(value){return true} else {return 'Please enter response to continue'}},
+        },
+    ]
+)
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
